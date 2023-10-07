7604705f832d87aa5958a7221caec2df5aff216e
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2023-10-07 14:04:52 +0000

    Basic HTTP server

---
+++ main.py
@@ -0,0 +1,127 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import typing
+import os
+import json
+
+hostName = "0.0.0.0"
+serverPort = 8093
+
+def read_file(filename):
+	f = open(filename, "r")
+	t = f.read()
+	f.close()
+	return t
+
+def bin_read_file(filename):
+	f = open(filename, "rb")
+	t = f.read()
+	f.close()
+	return t
+
+def write_file(filename, content):
+	f = open(filename, "w")
+	f.write(content)
+	f.close()
+
+class HttpResponse(typing.TypedDict):
+	status: int
+	headers: dict[str, str]
+	content: str | bytes
+
+class URLQuery:
+	def __init__(self, q: str):
+		self.fields: dict[str, str] = {}
+		for f in q.split("&"):
+			s = f.split("=")
+			if len(s) >= 2:
+				self.fields[s[0]] = s[1]
+	def get(self, key: str) -> str:
+		if key in self.fields:
+			return self.fields[key]
+		else:
+			return ''
+
+def get(path: str, query: URLQuery) -> HttpResponse:
+	if os.path.isfile("public_files" + path):
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": {
+					"html": "text/html",
+					"js": "text/javascript",
+					"css": "text/css"
+				}[path.split(".")[-1]]
+			},
+			"content": bin_read_file("public_files" + path)
+		}
+	elif os.path.isdir("public_files" + path):
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": read_file("public_files" + path + "index.html")
+		}
+	else: # 404 page
+		return {
+			"status": 404,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": f""
+		}
+
+def post(path: str, body: bytes) -> HttpResponse:
+	if False:
+		bodydata = body.decode("UTF-8").split("\n")
+	else:
+		return {
+			"status": 404,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": f""
+		}
+
+class MyServer(BaseHTTPRequestHandler):
+	def do_GET(self):
+		global running
+		splitpath = self.path.split("?")
+		res = get(splitpath[0], URLQuery('?'.join(splitpath[1:])))
+		self.send_response(res["status"])
+		for h in res["headers"]:
+			self.send_header(h, res["headers"][h])
+		self.end_headers()
+		c = res["content"]
+		if isinstance(c, str): c = c.encode("utf-8")
+		self.wfile.write(c)
+	def do_POST(self):
+		res = post(self.path, self.rfile.read(int(self.headers["Content-Length"])))
+		self.send_response(res["status"])
+		for h in res["headers"]:
+			self.send_header(h, res["headers"][h])
+		self.end_headers()
+		c = res["content"]
+		if isinstance(c, str): c = c.encode("utf-8")
+		self.wfile.write(c)
+	def log_message(self, format: str, *args) -> None:
+		return;
+		if 400 <= int(args[1]) < 500:
+			# Errored request!
+			print(u"\u001b[31m", end="")
+		print(args[0].split(" ")[0], "request to", args[0].split(" ")[1], "(status code:", args[1] + ")")
+		print(u"\u001b[0m", end="")
+		# don't output requests
+
+if __name__ == "__main__":
+	running = True
+	webServer = HTTPServer((hostName, serverPort), MyServer)
+	webServer.timeout = 1
+	print("Server started http://%s:%s" % (hostName, serverPort))
+	while running:
+		try:
+			webServer.handle_request()
+		except KeyboardInterrupt:
+			running = False
+	webServer.server_close()
+	print("Server stopped")
