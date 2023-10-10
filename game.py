96968487ce03fae1c9f67d2cb447ac88c1b279b7
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2023-10-10 01:51:42 +0000

    Added cool buttons

--- game.py
+++ game.py
@@ -3,9 +3,13 @@ class Action:
 		pass
 
 class Category:
-	def __init__(self, name):
+	def __init__(self, name: str):
 		self.name: str = name
-		self.items: list[str] = []
+		self.items: list[Item] = []
+
+class Item:
+	def __init__(self, name: str):
+		self.name: str = name
 
 class Game:
 	def __init__(self):
@@ -13,8 +17,10 @@ class Game:
 		self.categories: list[Category] = []
 		self.currentVote: None | Action = None
 		self.voteQueue: list[Action] = []
+		self.categories.append(Category("hi"))
+		for i in range(10): self.categories[0].items.append(Item(f"ASDFASDF{i}"))
 	def get_categories(self) -> list[dict[str, str | list[str]]]:
 		return [
-			{"name": c.name, "items": c.items}
+			{"name": c.name, "items": [x.name for x in c.items]}
 			for c in self.categories
 		]
--- public_files/script.js
+++ public_files/script.js
@@ -17,5 +17,57 @@ function request(url) {
 function refreshCategories() {
 	var data = request("/data/categories")
 	data.then((v) => {
+		/** @type {{items: string[], name: string}[]} */
+		var r = JSON.parse(v)
+		return r;
+	}).then((v) => {
+		for (var vi = 0; vi < v.length; vi++) {
+			var category = v[vi]
+			var e = document.createElement("div")
+			e.classList.add("category")
+			document.querySelector("#categories").appendChild(e)
+			// add header
+			var h = document.createElement("h5")
+			h.innerText = category.name
+			e.appendChild(h)
+			// add header buttons
+			for (var info of [
+				{ class: "remove" }
+			]) {
+				var b = document.createElement("button")
+				b.classList.add("button")
+				b.classList.add("btn-" + info.class)
+				h.appendChild(b)
+			}
+			// add spacer
+			var s = document.createElement("div")
+				s.classList.add("spacer")
+				e.appendChild(s)
+			// add items
+			for (var i = 0; i < category.items.length; i++) {
+				var s = document.createElement("button")
+				s.classList.add("new-btn")
+				s.setAttribute("onclick", `createNewItem(${vi}, ${i})`)
+				e.appendChild(s)
+				var s = document.createElement("button")
+				s.classList.add("delete-btn")
+				e.appendChild(s)
+				var s = document.createElement("button")
+				s.classList.add("move-btn")
+				e.appendChild(s)
+				// item
+				var i = document.createElement("div")
+				i.classList.add("item")
+				i.innerText = category.items[i]
+				e.appendChild(i)
+			}
+			var s = document.createElement("button")
+			s.classList.add("new-btn")
+			e.appendChild(s)
+		}
 	})
 }
+
+refreshCategories()
+
+function createNewItem(categoryno, itemno) {}
